cb96a6fe5e8e441aac2220c84ca33cb5ced3cfd8
wachuka7 <rachaelwachuka7@gmail.com>
2024-03-01 15:35:00 +0300

    feat: declare conditional statement

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -6,12 +6,20 @@ def admin_login(username, password):
     else: return "Access denied"
 
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature >40 and temperature<65:
+        return "It's a little chilly out there!"
+    elif temperature>85:
+        return "It's too dang hot out there!"
+    else: return "It's perfect out there!"
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3==0 and num%5==0:
+        return "FizzBuzz"
+    elif num % 3==0:
+        return "Fizz"
+    elif num%5==0:
+        return "Buzz"
+    else: return num
 
 def calculator(operation, num1, num2):
     # your code here
